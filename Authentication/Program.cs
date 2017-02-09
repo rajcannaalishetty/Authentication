c04431534fefabd7b94a1dfde472285acdd9bee6
rajcannaalishetty <rajcanna.a@gmail.com>
2017-02-09 15:38:00 -0600

    when user enters wrong password for 2 times ...then account gets blocked

--- Authentication/Program.cs
+++ Authentication/Program.cs
@@ -1,15 +1,43 @@
 ﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
+
 
 namespace Authentication
 {
     class Program
     {
+
         static void Main(string[] args)
         {
+            int i = 0;
+
+        TryAgain:
+            Console.WriteLine("\nenter your username ");
+            string user = Console.ReadLine().ToUpper();
+            Console.WriteLine("\nenter your password");
+            string password = (Console.ReadLine().ToUpper());
+
+            if (user == "RAJCANNA" && password == "RAJ18110")
+            {
+                Console.WriteLine("login successful \n\n*****enjoy your day*****");
+
+            }
+            else
+
+                do
+                {
+                    while (i < 2)
+                    {
+                        Console.WriteLine("I'm sorry ..something went wrong..please try again");
+                        Console.WriteLine("\n\nnumber of attempts left to u are {0}", 2 - i);
+                        i++;
+                        goto TryAgain;
+
+                    }
+                    Console.WriteLine("user account has been blocked");
+                    Console.ReadLine();
+                } while (i < 2);
+
         }
+
     }
 }
